5ad12ff008e6ded08b2aead66ae3685d19704071
LeeJinKyum <109436439+LeeJinKyum@users.noreply.github.com>
2022-07-23 03:17:26 +0900

    JavaScript

---
+++ app.js
@@ -0,0 +1,10 @@
+const player = {
+    name: "nico",
+    sayHello: function(otherPersonsName){
+        console.log("hello " + otherPersonsName
+        + " nice meet you!");
+    },
+};
+
+player.sayHello("은정");
+player.sayHello("진겸");
